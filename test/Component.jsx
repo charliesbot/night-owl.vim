7ac8725057edc34cd865fdd31c5ebb9502c299ef
Charlie L <charliesbot@gmail.com>
2020-06-11 19:41:07 -0700

    base implementation

---
+++ test/Component.jsx
@@ -0,0 +1,44 @@
+const EmailField = function (props) {
+  return {
+    propTypes: {
+      email: React.PropTypes.string,
+      isEditMode: React.PropTypes.bool,
+    },
+
+    props,
+
+    onKeyUp(e) {
+      if (e.keyCode !== ENTER_KEY) return;
+      setEmail(e.target.value);
+    },
+
+    render() {
+      const { a, b } = c;
+      let hi = "hi";
+      const isEditMode = this.props.isEditMode;
+      const email = this.props.email;
+
+      const displayStyle = {
+        display: isEditMode ? "none" : "block",
+      };
+      const editStyle = {
+        display: isEditMode ? "block" : "none",
+      };
+
+      return (
+        <div>
+          <p onClick={() => setEditMode(true)} style={displayStyle}>
+            {email}
+          </p>
+          <Component />
+          <input
+            type="email"
+            onKeyUp={this.onKeyUp}
+            style={editStyle}
+            placeholder={email}
+          />
+        </div>
+      );
+    },
+  };
+};
---
+++ test/Component.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+type Props = {
+  isFullScreen: boolean;
+  username: string;
+};
+
+const Custom: React.FC<Props> = (props) => {
+  return (
+    <div>
+      <Animated />
+    </div>
+  );
+};
+
+export { Custom };
+
